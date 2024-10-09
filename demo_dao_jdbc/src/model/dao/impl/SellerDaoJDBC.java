b21dae8309bf6814e49e242b992c334b693091af
Matheus Farias <matheusaf.contato@gmail.com>
2024-10-09 09:45:53 -0300

    delete implementation

--- demo_dao_jdbc/src/model/dao/impl/SellerDaoJDBC.java
+++ demo_dao_jdbc/src/model/dao/impl/SellerDaoJDBC.java
@@ -89,7 +89,27 @@ public class SellerDaoJDBC implements SellerDao {
 
 	@Override
 	public void deleteById(Integer id) {
-		// TODO Auto-generated method stub
+		
+		PreparedStatement st = null;
+		try {
+			st = conn.prepareStatement(
+					"DELETE FROM coursejdbc.seller "
+					+ "WHERE Id = ?");
+			
+			st.setInt(1, id);
+			
+			int rows = st.executeUpdate();
+			
+			if(rows == 0) {
+				System.out.println("the ID entered does not exist.");
+			}
+		}
+		catch(SQLException e) {
+			throw new DbException(e.getMessage());
+		}
+		finally {
+			DB.closeStatement(st);
+		}
 
 	}
 
